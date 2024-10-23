2d23852b2c95630d63c72295690704cb13aad000
Marek Loose <loose.marek@gmail.com>
2024-10-23 20:52:25 +0200

    Added Common module

---
+++ Common/Common/Common.h
@@ -0,0 +1,18 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Marek Loose on 23/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
