301618c3fbfa1fad05edcb1e7c88b6331691ed64
Marek Loose <loose.marek@gmail.com>
2024-10-23 20:48:18 +0200

    Added Workspace and main app

---
+++ ModularApp/ModularApp/ModularAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularAppApp.swift
+//  ModularApp
+//
+//  Created by Marek Loose on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ModularAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
