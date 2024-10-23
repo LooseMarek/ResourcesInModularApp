f14887c439baf543952a6d6e62716930ce6e3157
Marek Loose <loose.marek@gmail.com>
2024-10-23 21:32:46 +0200

    Linked modules. Used color and image from Common

--- ModularApp/ModularApp/ContentView.swift
+++ ModularApp/ModularApp/ContentView.swift
@@ -6,14 +6,15 @@
 //
 
 import SwiftUI
+import Common
+import UIComponents
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Text Color from Common")
+                .background(Color.myCustomColor)
+            ExampleComponent()
         }
         .padding()
     }
--- UIComponents/UIComponents/ExampleComponent.swift
+++ UIComponents/UIComponents/ExampleComponent.swift
@@ -6,13 +6,20 @@
 //
 
 import SwiftUI
+import Common
 
 public struct ExampleComponent: View {
     
+    public init() {}
+    
     public var body: some View {
         VStack {
-            Text("Example")
-                .tint(.blue)
+            Text("UIComponents Text with color from Common")
+                .background(Color.myCustomColor)
+            Image.myCustomImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
         }
     }
 }
